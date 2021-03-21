53d4e69e8fea63dd92058aedb2dd2e6a693927e4
giovannetor <giovanni.coci1@yahoo.com>
2021-03-21 10:12:54 +0100

    commented and fixed a minor issue

--- easter.py
+++ easter.py
@@ -1,5 +1,5 @@
 import sopel.module as module
-import sopel.tools as tools
+import sopel.tools as tools     #import the 2 sopel modules required
 
 
 strings = {
@@ -8,44 +8,45 @@ strings = {
     "left" : "Ti sono rimasti %s tentativi / You've got %s attempts left.",
     "adminegg" : "Hai ricevuto un uovo da %s. / You received an egg from %s . https://usercontent.twoopy.nl/6b4f281c5a446791/image.png",
     "admineggself" : "Ti sei dato un uovo da solo %s (monello!)"
-}
+}   # The bot is designed to stay in #italiano, but we expect people from everywhere to come to find the egg. The strings
+    # are both in Italian and English.
 
-class easteregg:
+class easteregg:    # everything is inside a single little class
     
     def __init__(self):
-        self.user = {}
+        self.user = {}   # this dict will store the user's nick of whoever uses the .easteregg command. Being a dict
+                         # inside the code and not in a db, the datas will vanish if the bot will reload.
 
-    def egg(self , bot , trigger):
-        if trigger.nick not in self.user:
+    def egg(self , bot , trigger):     # main function of the bot. triggered by .easteregg , insert the trigger in
+        if trigger.nick not in self.user:  # self.user and starts the counter.
             self.user[trigger.nick] = 0
         if self.user[trigger.nick] >= 3:
-            bot.notice(strings["limite"] , trigger.nick)
-            return
+            bot.notice(strings["limite"] , trigger.nick)  # the limit of 3 eggs is to prevent flood in channel. anyway,
+            return                                        # the image will be provided via notice.
         self.user[trigger.nick] += 1
         bot.notice(strings["egg"] % (trigger.nick) , trigger.nick)
         bot.notice(strings["left"] % ( str(3 - self.user[trigger.nick]) , str(3 - self.user[trigger.nick])) , trigger.nick)
 
-    def adminegg(self , bot , trigger):
+    def adminegg(self , bot , trigger): # for admins only. in case of need, an admin can manually give an egg to a user.
         try:
            bot.notice(strings["adminegg"] % (trigger.nick , trigger.nick) , trigger.group(3))
         except:
-           bot.notice(strings["admineggself"] % (trigger.nick) , trigger.nick)
-        bot.notice("Uovo consegnato correttamente :P" , trigger.nick)
+           bot.notice(strings["admineggself"] % (trigger.nick) , trigger.nick)  # using .giveegg without argument will result
+        bot.notice("Uovo consegnato correttamente :P" , trigger.nick)          # in a self given egg.
 
 
 eggita = easteregg()
 
 
-@module.commands("easteregg")
-@module.example(".easteregg")
+@module.commands("easteregg")    # this command might change it's syntax in the future (maybe a code to find trough some
+@module.example(".easteregg")    # activity)
 @module.require_chanmsg
 def eegg(bot , trigger):
-    eggita.egg(bot , trigger)
+    eggita.egg(bot , trigger)    # all the commands require chan message (we don't like private chats)
 
 @module.commands("giveegg")
-@module.example(".giveegg giovannetor")
-
-@module.require_chanmsg
+@module.example(".giveegg giovannetor , .giveegg")  # the command for admins. If the user is not admin, the bot replies
+@module.require_chanmsg                             # with a smily face :)
 def giveegg(bot , trigger):
     if trigger.admin:
         eggita.adminegg(bot , trigger)
